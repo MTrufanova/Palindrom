e56ca8c5551d8d8cc383fab0f3342bad8847e508
Marina Trufanova <trufanovamyu@gmail.com>
2021-05-31 21:20:46 +0300

    bool palindrome func

---
+++ Palindrom/MainViewController.swift
@@ -0,0 +1,112 @@
+//
+//  ViewController.swift
+//  Palindrom
+//
+//  Created by msc on 31.05.2021.
+//
+
+import UIKit
+import SnapKit
+
+final class MainViewController: UIViewController {
+
+    //MARK: - UI
+    
+    lazy var inputTextView: UITextView = {
+       let textView = UITextView()
+       textView.text = "Input text here"
+        textView.font = .systemFont(ofSize: 16, weight: .regular)
+        textView.contentInset = UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 0)
+        textView.layer.cornerRadius = 10
+        textView.layer.borderColor = UIColor.gray.cgColor
+        textView.layer.borderWidth = 1
+        textView.textColor = .lightGray
+        textView.backgroundColor = .white
+        textView.delegate = self
+        return textView
+    }()
+    
+    lazy var searchButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .black
+        button.setTitle("Search", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 25
+        button.addTarget(self, action: #selector(searchPalindrome), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var outputTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = .systemFont(ofSize: 16, weight: .regular)
+        textView.contentInset = UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 0)
+        textView.layer.cornerRadius = 10
+        textView.layer.borderColor = UIColor.gray.cgColor
+        textView.layer.borderWidth = 1
+        textView.textColor = .black
+        textView.isEditable = false
+        textView.isHidden = true
+         return textView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Search Palindrom"
+        setupLayout()
+       
+    }
+
+    //MARK: - Methods
+    
+    @objc func searchPalindrome() {
+        if inputTextView.text != nil && inputTextView.text != "Input text here" {
+            outputTextView.isHidden = false
+            let input = inputTextView.text.lowercased().replacingOccurrences(of: " ", with: "")
+            
+            if String(input.reversed()) == input {
+                outputTextView.text = "This is palindrome"
+            } else {
+                outputTextView.text = "This isn't palindrome"
+            }
+        }
+    }
+    
+    //MARK: - Layout
+    
+    private func setupLayout() {
+        view.addSubview(inputTextView)
+        view.addSubview(searchButton)
+        view.addSubview(outputTextView)
+        
+        inputTextView.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(150)
+            make.leading.trailing.equalToSuperview().inset(8)
+            make.height.equalTo(150)
+        }
+        searchButton.snp.makeConstraints { (make) in
+            make.top.equalTo(inputTextView.snp.bottom).offset(16)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(150)
+            make.height.equalTo(50)
+            
+        }
+        
+        outputTextView.snp.makeConstraints { (make) in
+            make.top.equalTo(searchButton.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview().inset(8)
+            make.height.equalTo(150)
+        }
+    }
+
+}
+
+//MARK: - UITextViewDelegate
+
+extension MainViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        inputTextView.text = ""
+        inputTextView.textColor = .black
+    }
+    
+    
+}
